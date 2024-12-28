c6f249642f22b71d8f45a5a5faf61c3ccbd9fc2d
Ariel <arielc.nunes@outlook.com>
2024-12-28 17:52:59 +0000

    Added links to home page and create a grades page that display students grades by joining MySQL tables

--- src/index-MacBook Pro.js
+++ src/index-MacBook Pro.js
@@ -10,14 +10,10 @@ app.set("view engine", "ejs");
 
 // Main Page
 app.get("/", (req, res) => {
-  res.render("mainPage", {
-    studentLink: "/students",
-    gradeLink: "/grades",
-    lecturerLink: "/lecturers",
-  });
+  res.render("mainPage", { studentLink: "/students", gradeLink: "/grades", lecturerLink: "/lecturers" });
 });
 
-// STUDENTS FROM MYSQL
+// STUDENTS DATABASE
 app.get("/students", async (req, res) => {
   try {
     const pool = await poolPromise;
@@ -29,28 +25,18 @@ app.get("/students", async (req, res) => {
   }
 });
 
-// GRADES FROM MYSQL
 app.get("/grades", async (req, res) => {
   try {
     const pool = await poolPromise;
-    const grade = await pool.query(`
-      SELECT 
-        s.name AS sName, 
-        m.name AS mName, 
-        g.grade AS grade
-      FROM grade g
-      JOIN student s ON g.sid = s.sid
-      JOIN module m ON g.mid = m.mid
-      ORDER BY s.name ASC, g.grade ASC
-    `);
-    res.render("grades", { grade: grade });
+    const students = await pool.query("SELECT * FROM student");
+    res.render("students", { students: students });
   } catch (err) {
     console.error(err);
     res.status(500).send("Database error");
   }
 });
 
-// LECTURERS FROM JSON
+// LECTURERS JSON
 app.get("/lecturers", (req, res) => {
   mongo
     .findAll()
